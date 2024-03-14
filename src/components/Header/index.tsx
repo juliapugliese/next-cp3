158ffff5d47a4f9d17ed97be342e11b8e8c1f5b1
Julia <rm553427@fiap.com.br>
2024-03-14 09:53:31 -0300

    Estilização dos Componente Header e Footer

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css"
+
+const Header  = () => {
+    return( 
+    <>
+        <header>
+            <img src="/channels4_profile.jpg" alt="Logo NetMovies" className="logo"/>
+        
+            <p>Os melhores filmes estão aqui</p>
+            
+        </header>
+    </>);
+};
+
+export default Header;
