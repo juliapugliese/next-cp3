a1152a44e4f069b539d522f6255a64ecaae3b7a3
Julia Pugliese <julia@jujubooks.com.br>
2024-03-15 18:11:44 -0300

    Criação e utilização do componente CardFilm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,67 +12,67 @@ export default function Home() {
       <CardFilm 
       title="Barbie"
       image="/barbie.avif"
-      link=""
+      link="https://www.max.com/br/pt/movies/barbie/80bc4915-c826-499f-9961-b422b17559b6"
       ></CardFilm>
 
       <CardFilm 
       title="Kung Fu Panda"
       image="/kung-fu-panda.jpg"
-      link=""
+      link="https://www.youtube.com/watch?v=_inKs4eeHiI"
       ></CardFilm>
 
       <CardFilm 
       title="Patos"
       image="/patos.jpg"
-      link=""
+      link="https://www.youtube.com/watch?v=1ytJRfknMhA"
       ></CardFilm>
 
       <CardFilm 
       title="9-a salvação"
       image="/9-salvacao.jpg"
-      link=""
+      link="https://www.max.com/br/pt/movies/9-a-salvacao/9a9d67f4-f2ff-44fc-ae28-fc212f55b95f"
       ></CardFilm>
 
       <CardFilm 
       title="Bolt - Supercão"
       image="/bolt.webp"
-      link=""
+      link="https://www.disneyplus.com/pt-br/movies/bolt/3rv7GKMZTggh"
       ></CardFilm>
 
       <CardFilm 
       title="Coraline e o Mundo Secreto"
       image="/coraline.jpg"
-      link=""
+      link="https://www.youtube.com/watch?v=bsgxE1TjgiM"
       ></CardFilm>
 
       <CardFilm 
       title="O Corajoso Ratinho Despereaux"
       image="/despereaux.jpg"
-      link=""
+      link="https://www.youtube.com/watch?v=0kGoA_Xc5L0"
       ></CardFilm>
 
       <CardFilm 
       title="Um Lugar Silencioso"
       image="/lugar-silencioso.webp"
-      link=""
+      link="https://play.google.com/store/movies/details?id=u2L8EchdvKw.P"
       ></CardFilm>
 
       <CardFilm 
       title="Procurando Nemo"
       image="/nemo.webp"
-      link=""
+      link="https://www.disneyplus.com/pt-br/movies/finding-nemo/5Gpj2XqF7BV2"
       ></CardFilm>
    
       <CardFilm 
       title="Oportunistas"
       image="/oportunistas.webp"
-      link=""
+      link="https://play.google.com/store/movies/details/Oportunistas_Dublado?id=s4VwJDlCYeA&hl=pt&gl=US"
       ></CardFilm>
 
       <CardFilm 
       title="Tainá"
       image="/taina.jpg"
-      link=""
+      link="https://www.primevideo.com/-/pt/detail/Tain%C3%A1-Uma-Aventura-na-Amaz%C3%B4nia/0TTK8RL0CNZYRC9GT2NUL8B508"
       ></CardFilm>
     </main>
   );
